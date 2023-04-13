4c732c7b1f44117db5102421c9772c56f1757c04
ulfhiraa <studyiraa@gmail.com>
2023-04-13 11:14:00 +0700

    masuk angka dari elemen pada array, jika n lebih besar dari 0 dan n kurang dari sama dengan 20 maka selesai. jika tidak, akan muncul "array harus memiliki minimum 1 dan maksimum 20 elemen)

--- LinearSearch_093/LinearSearch_093.cpp
+++ LinearSearch_093/LinearSearch_093.cpp
@@ -6,7 +6,20 @@ int n;           // Number of elements in the array
 int i;           // Index of array element 
 
 void input()
-
 {
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+
+	}
+
+
+
+
 
 }
