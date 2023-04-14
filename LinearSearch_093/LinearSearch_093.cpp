22e00fbf53963762239e1c762744b42710722ec3
ulfhiraa <studyiraa@gmail.com>
2023-04-14 15:08:03 +0700

    langkah 2,3,4 pada prosedur LinearSearch

--- LinearSearch_093/LinearSearch_093.cpp
+++ LinearSearch_093/LinearSearch_093.cpp
@@ -15,13 +15,12 @@ void input()
 			break;
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
-
 	}
 
 	// Accept array elements
-	cout << "\n----------------------\n";
-	cout << "\enter array elements \n";
-	cout << "------------------------\n";
+	cout << "\n--------------------\n";
+	cout << " Enter array elements \n";
+	cout << "----------------------\n";
 	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << ">";
@@ -32,33 +31,33 @@ void input()
 void LinearSearch()
 {
 	char ch;
-	int comparasion;       // Number of comparasions
+	int comparasions;       // Number of comparasions
 
-	do																			  // Langkah 3
+	do																			  
 	{
 		   // Accept the number to be searched
-		cout << "\n Enter the element you want to search";							 // Langkah 1
+		cout << "\nEnter the element you want to search: ";							 // Langkah 1
 		int item;
 		cin >> item;
 
-		comparasion = 0;
-		for (i = 0; i < n; i++)													// langkah 2 dan 4
+		comparasions = 0;
+		for (i = 0; i < n; i++)													// langkah 2,3 dan 4
 		{
-			comparasion++;
-			if (arr[i] == item)														// langkah 5 a found
+			comparasions++;
+			if (arr[i] == item)														// langkah 5A found
 			{
 				cout << "\n" << item << " Found at position " << (i + 1) << endl;
 				break;
 			}
 		}
 
-		if (i == n)														 // Langkah 5 b not found
+		if (i == n)														 // Langkah 5B not found
 			cout << "\n" << item << " not found in the array\n";
-		cout << "\nNumber of comparasions : " << comparasion << endl;
+		cout << "\nNumber of comparasions : " << comparasions << endl;
 
 		cout << "\nContinue search (y/n): ";
 		cin >> ch;
-	} while ((ch == 'y') || (ch == 'y'));
+	} while ((ch == 'y') || (ch == 'Y'));
 }
 
 int main()
