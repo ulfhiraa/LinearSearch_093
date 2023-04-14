c658a02c212925b23876d5846705108b2114a69a
ulfhiraa <studyiraa@gmail.com>
2023-04-14 12:57:59 +0700

    proses looping

--- LinearSearch_093/LinearSearch_093.cpp
+++ LinearSearch_093/LinearSearch_093.cpp
@@ -22,8 +22,9 @@ void input()
 	cout << "\n----------------------\n";
 	cout << "\enter array elements \n";
 	cout << "------------------------\n";
-	
-
-
-
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
